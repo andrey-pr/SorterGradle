12652dd40e8c41fc9f9adff7c541c36414f2704b
Andrey <andreykvak.g3@gmail.com>
2020-08-26 17:39:56 +0300

    Made InsertionSort

---
+++ src/main/java/dev/scud/sorter/InsertionSort.java
@@ -0,0 +1,17 @@
+package dev.scud.sorter;
+
+import java.util.Comparator;
+
+public class InsertionSort implements Sorter{
+    @Override
+    public <T> void sort(T[] arr, Comparator<T> c) {
+        for(int i = 1; i < arr.length; i++)
+            for(int a = i; a > 0; a--)
+                if(c.compare(arr[a], arr[a-1]) < 0)
+                {
+                    T buff = arr[a];
+                    arr[a] = arr[a-1];
+                    arr[a-1] = buff;
+                }
+    }
+}
--- src/test/java/dev/scud/sorter/SorterTest.java
+++ src/test/java/dev/scud/sorter/SorterTest.java
@@ -15,19 +15,48 @@ public class SorterTest {
 
     private Sorter subject;
 
-    public SorterTest(Sorter subject){
+    public SorterTest(Sorter subject) {
         this.subject = subject;
     }
 
     @Parameterized.Parameters
     public static Collection<Object[]> getParameters() {
-        return Arrays.asList(new Object[][] {
-                { new BubbleSort() }
+        return Arrays.asList(new Object[][]{
+                {new BubbleSort()},
+                {new InsertionSort()}
         });
     }
 
     @Test
-    public void sort() {
+    public void sort1() {
+        Integer[] arr = new Integer[]{5};
+        subject.sort(arr, Comparator.comparingInt((Integer a) -> a));
+        assertArrayEquals(new Integer[]{5}, arr);
+    }
+
+    @Test
+    public void sort2() {
+        Integer[] arr = new Integer[]{5, 3};
+        subject.sort(arr, Comparator.comparingInt((Integer a) -> a));
+        assertArrayEquals(new Integer[]{3, 5}, arr);
+    }
+
+    @Test
+    public void sort3() {
+        Integer[] arr = new Integer[]{5, 200};
+        subject.sort(arr, Comparator.comparingInt((Integer a) -> a));
+        assertArrayEquals(new Integer[]{5, 200}, arr);
+    }
+
+    @Test
+    public void sort4() {
+        Integer[] arr = new Integer[]{2, 3, 4, 5, 200};
+        subject.sort(arr, Comparator.comparingInt((Integer a) -> a));
+        assertArrayEquals(new Integer[]{2, 3, 4, 5, 200}, arr);
+    }
+
+    @Test
+    public void sort5() {
         Integer[] arr = new Integer[]{5, 200, 4, 2, 3};
         subject.sort(arr, Comparator.comparingInt((Integer a) -> a));
         assertArrayEquals(new Integer[]{2, 3, 4, 5, 200}, arr);
